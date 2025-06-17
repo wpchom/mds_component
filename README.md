fd9a71cba351e39e0404c807cef6e2e5c8802bdb
pchom <pchom@hotmail.com>
2025-06-18 01:50:49 +0800

    feat(component): create new repository for mds_component
    - Add .clang-format file for code formatting
    - Add .gitignore file for version control
    - Create new directory structure for component
    - Add build configuration files (BUILD.gn)
    - Add header files for various modules (algo, boot, button, emfs)
    - Rename existing header files to new location
    - Update function declarations in header files
    - Remove extern keyword from function declarations

---
+++ README.md
@@ -0,0 +1,16 @@
+# MDS Component
+
+component for mds_kernel and mds_device
+
+
+# roadmap
+- [ ] compress_log
+- [ ] fixup filesystem
+- [ ] add rawfs / ringfs
+- [ ] update nvs feature to emfs/nvfs
+- [ ] at command
+- [ ] logger thread
+- [ ] libc redirect
+- [ ] add shell/cli
+- [ ] support lvgl
+- [ ] support cherryusb
---
+++ inc/fs/mds_emfs.h
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) [2022] [pchom]
+ * [MDS_FS] is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ **/
+#ifndef __MDS_EMFS_H__
+#define __MDS_EMFS_H__
+
+/* Include ----------------------------------------------------------------- */
+#include "dev_storage.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Typedef ----------------------------------------------------------------- */
+typedef uint16_t MDS_EMFS_FileId_t;
+
+typedef struct MDS_EMFS_FsInitStruct {
+    DEV_STORAGE_Periph_t *device;
+    uint8_t *buff;
+    size_t size;
+} MDS_EMFS_FsInitStruct_t;
+
+typedef struct MDS_EMFS_FileSystem {
+    MDS_ListNode_t list;
+    MDS_Mutex_t mutex;
+    size_t pageSize;
+    size_t readOfs;
+    size_t writeOfs;
+
+    MDS_EMFS_FsInitStruct_t init;
+} MDS_EMFS_FileSystem_t;
+
+typedef struct MDS_EMFS_FileDesc {
+    MDS_ListNode_t node;
+    MDS_EMFS_FileSystem_t *fs;
+    uint8_t *data;
+} MDS_EMFS_FileDesc_t;
+
+/* Function ---------------------------------------------------------------- */
+MDS_Err_t MDS_EMFS_Mkfs(DEV_STORAGE_Periph_t *device, size_t sctsz);
+MDS_Err_t MDS_EMFS_Mount(MDS_EMFS_FileSystem_t *fs, const MDS_EMFS_FsInitStruct_t *init);
+MDS_Err_t MDS_EMFS_Unmout(MDS_EMFS_FileSystem_t *fs);
+
+MDS_EMFS_FileDesc_t *MDS_EMFS_FileGetOpened(MDS_EMFS_FileSystem_t *fs, MDS_EMFS_FileId_t id);
+MDS_Err_t MDS_EMFS_Remove(MDS_EMFS_FileSystem_t *fs, MDS_EMFS_FileId_t id);
+MDS_Err_t MDS_EMFS_Rename(MDS_EMFS_FileSystem_t *fs, MDS_EMFS_FileId_t oldid,
+                          MDS_EMFS_FileId_t newid);
+MDS_Err_t MDS_EMFS_FileSize(MDS_EMFS_FileDesc_t *fd, size_t *datasz, size_t *size);
+
+MDS_Err_t MDS_EMFS_FileCreate(MDS_EMFS_FileDesc_t *fd, MDS_EMFS_FileSystem_t *fs,
+                              MDS_EMFS_FileId_t id);
+MDS_Err_t MDS_EMFS_FileOpen(MDS_EMFS_FileDesc_t *fd, MDS_EMFS_FileSystem_t *fs,
+                            MDS_EMFS_FileId_t id);
+MDS_Err_t MDS_EMFS_FileClose(MDS_EMFS_FileDesc_t *fd);
+size_t MDS_EMFS_FileTruncate(MDS_EMFS_FileDesc_t *fd, intptr_t ofs);
+MDS_Err_t MDS_EMFS_FileRead(MDS_EMFS_FileDesc_t *fd, intptr_t ofs, uint8_t *buff, size_t size,
+                            size_t *read);
+MDS_Err_t MDS_EMFS_FileWrite(MDS_EMFS_FileDesc_t *fd, intptr_t ofs, const uint8_t *buff,
+                             size_t size, size_t *write);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MDS_EMFS_H__ */
